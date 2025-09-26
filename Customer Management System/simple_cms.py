add9fb37bb329c22de6ed5ba4a6c16d5eb59c4b3
Nitil <nitil@gmail.com>
2025-09-26 15:29:50 +0530

    update 1

--- Customer Management System/simple_cms.py
+++ Customer Management System/simple_cms.py
@@ -5,11 +5,23 @@ name_list=[]
 age_list=[]
 mob_list=[]
 
+def add_Customer(id,name,age,mob):
+    id_list.append(id)
+    name_list.append(name)
+    age_list.append(age)
+    mob_list.append(mob)
+    return
 
+def search_Customer(id):
+    index=id_list.index(id)
+    return index
 
 
 # Presentation Layer
 print('Welcome to Customer Management System')
+
+def show_Customer(i):
+    print('Cust ID:',id_list[i],'Cust Name:',name_list[i],'Cust Age:',age_list[i],'Cust Mob:',mob_list[i])
 
 
 
@@ -20,4 +32,10 @@ while(1):
         name=input('Enter Customer Name: ')
         age=input('Enter Customer Age: ')
         mob=input('Enter Customer Mobile Number: ')
-        
+        add_Customer(id,name,age,mob)
+        print('Customer Added Successfully')
+    
+    elif ch=='2':
+        id=input('Enter Id to Search Customer: ')
+        i=search_Customer(id)
+        show_Customer(i)
