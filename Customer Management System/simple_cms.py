6f95ba3c045e16af2346f03df78e9bd12c774c43
Nitil <nitil@gmail.com>
2025-09-26 22:35:13 +0530

    update 2

--- Customer Management System/simple_cms.py
+++ Customer Management System/simple_cms.py
@@ -16,6 +16,20 @@ def search_Customer(id):
     index=id_list.index(id)
     return index
 
+def delete_Customer(id):
+    index=id_list.index(id)
+    id_list.pop(index)
+    name_list.pop(index)
+    age_list.pop(index)
+    mob_list.pop(index)
+    return
+
+def modify_Customer(id,name,age,mob):
+    i=id_list.index(id)
+    name_list[i]=name
+    age_list[i]=age
+    mob_list[i]=mob
+    return
 
 # Presentation Layer
 print('Welcome to Customer Management System')
@@ -39,3 +53,29 @@ while(1):
         id=input('Enter Id to Search Customer: ')
         i=search_Customer(id)
         show_Customer(i)
+    
+    elif ch=='3':
+        id=input('Enter ID to Delete Customer: ')
+        delete_Customer(id)
+        print('Customer Deleted Successfully')
+
+    elif ch=='4':
+        id=input('Enter Customer ID to Update: ')
+        name=input('Enter Customer Name to Update: ')
+        age=input('Enter Customer Age to Update: ')
+        mob=input('Enter Customer Mobile Number to Update: ')
+        modify_Customer(id,name,age,mob)
+        print('Customer Modified Successfully')
+    
+    elif ch=='5':
+        for i in range(len(id_list)):
+            show_Customer(i)
+    
+    elif ch=='6':
+        print('Thank You for using Customer Management System')
+        break
+    
+    else:
+        print('Incorrect Choice')
+
+
